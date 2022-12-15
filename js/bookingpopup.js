30bf24f2e1f9937e637ecf386676a200cd72018e
Hải Sơn <haison@MacBook-Pro-cua-Hai.local>
2022-12-15 21:30:54 +0700

    4th commit

--- js/bookingpopup.js
+++ js/bookingpopup.js
@@ -6,3 +6,9 @@ document.querySelector(".close-btn").addEventListener("click", function() {
     document.querySelector(".popup").classList.remove("active2");
     document.querySelector(".background-popup").classList.remove("active1");
 });
+
+document.querySelector(".form-element button").addEventListener("click", function(){
+    alert('Đặt lịch khám thành công');
+    document.querySelector(".popup").classList.remove("active2");
+    document.querySelector(".background-popup").classList.remove("active1");
+});
