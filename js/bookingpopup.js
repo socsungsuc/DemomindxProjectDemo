3a1754d51aab9d6a202a1de6cd8ae65d07fe4934
Hải Sơn <haison@MacBook-Pro-cua-Hai.local>
2022-12-16 23:19:30 +0700

    4rd commit

--- js/bookingpopup.js
+++ js/bookingpopup.js
@@ -2,13 +2,89 @@ document.querySelector("#show-booking").addEventListener("click", function() {
     document.querySelector(".popup").classList.add("active2");
     document.querySelector(".background-popup").classList.add("active1");
 });
-document.querySelector(".close-btn").addEventListener("click", function() {
+document.querySelector(".popup .close-btn").addEventListener("click", function() {
     document.querySelector(".popup").classList.remove("active2");
     document.querySelector(".background-popup").classList.remove("active1");
 });
 
-document.querySelector(".form-element button").addEventListener("click", function(){
-    alert('Đặt lịch khám thành công');
-    document.querySelector(".popup").classList.remove("active2");
-    document.querySelector(".background-popup").classList.remove("active1");
+// document.querySelector(".form-element button").addEventListener("click", function(){
+//     document.querySelector(".popup").classList.remove("active2");
+
+//     document.querySelector(".popup-alert").classList.add("active3");
+// });
+// document.querySelector(".popup-alert .close-btn").addEventListener("click", function(){
+//     document.querySelector(".popup-alert").classList.remove("active3");
+//     document.querySelector(".background-popup").classList.remove("active1");
+// });
+
+var username = document.querySelector("#name");
+var useremail = document.querySelector("#email");
+var bookingfor = document.querySelector("#bookingfor");
+var userdate = document.querySelector("#bookingdate");
+var userform = document.querySelector('form');
+var usersubmit = document.querySelector('button');
+
+function showError(input, message){
+    let parent = input.parentElement;
+    let small = parent.querySelector('small');
+    parent.classList.add("error");
+    small.innerText = message;
+}
+function showSuccess(input){
+    let parent = input.parentElement;
+    let small = parent.querySelector('small');
+    input.parentElement.classList.remove("error");
+    small.innerText = '';
+}
+
+function checkEmptyError(listInput){
+    let isEmptyError = false;
+    listInput.forEach(input => {
+        input.value = input.value.trim();
+
+        if (!input.value){
+            isEmptyError = true;
+            showError(input, 'Khong duoc de trong');
+        } else{
+            showSuccess(input);
+        }
+    });
+    return isEmptyError;
+}
+
+function checkEmail(input){
+    const regexEmail = '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
+    input.value = input.value.trim()
+
+    if (regexEmail.test(input.value)){
+        showSuccess(input)
+    } else{
+        showError(input, 'Email khong hop le')
+    }
+
+    return regexEmail.test(input.value);
+}
+
+function bookingdate(){
+    let a = document.getElementById("bookingdate").value;
+    document.getElementById("res").innerHTML = 'Hẹn gặp lại quý khách vào ngày ' + a;
+}
+
+usersubmit.addEventListener('click', function(e){
+    e.preventDefault();
+    let isEmptyError = checkEmptyError([username, useremail, bookingfor, userdate]);
+    // let isEmailError = checkEmail(useremail);
+
+    if (isEmptyError)
+    {
+           
+    } else{
+            bookingdate();
+            document.querySelector(".popup").classList.remove("active2");
+            document.querySelector(".popup-alert").classList.add("active3");
+        document.querySelector(".popup-alert .close-btn").addEventListener("click", function(){
+            document.querySelector(".popup-alert").classList.remove("active3");
+            document.querySelector(".background-popup").classList.remove("active1");
+        });
+    }
 });
--- js/ketnoisql.js
+++ js/ketnoisql.js
@@ -2,7 +2,7 @@ var khoa = 1;
 var id_goix = 0;
 var kq_output = 0;
 var cap_nhat = 0;
-var name,email,subject_f,message_f;
+// let name,email,subject_f,message_f;
 
 function handleSendData(){
    
@@ -14,20 +14,20 @@ function handleSendData(){
     {
      if (cap_nhat == 0) {
 	document.getElementById("button_sub").innerHTML = "Đang gửi dữ liệu về Server";
-     name = form.elements.name.value;
-     email = form.elements.email.value;
-     subject_f = form.elements.subject.value;
-     message_f = form.elements.message.value;
-	 var luu_local = name + ", " + email + ", " + subject_f + ", " +  message_f;
-	 localStorage.setItem("thongtin", luu_local);
+     let name = form.elements.name.value;
+     let email = form.elements.email.value;
+     let subject_f = form.elements.subject.value;
+     let message_f = form.elements.message.value;
+	var luu_local = name + ", " + email + ", " + subject_f + ", " +  message_f;
+	localStorage.setItem("thongtin", luu_local);
     
 	
      khoa = 0;
      id_goix = 1;
      kq_output = 0;
-	 cap_nhat = 2;
+	cap_nhat = 2;
 	 
-    }
+}
     
-    }     
-    }
+}     
+}
